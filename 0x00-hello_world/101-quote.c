dd98be674c6d21a866d1013499d03ba567ad9693
Cabdalle <abnagsh@gmail.com>
2022-06-02 17:33:01 -0700

    Question 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,12 @@
 #include <unistd.h>
 /**
  * main -start point
- * Description:no print
- * Return: return 1 means succes
+ *Return: 1
  */
+
 int main(void)
 {
-	char str1[] = "and that piece of art is useful\"
-		- Dora Korpar,2015-10-19\n";
-write(2, str1, 59);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
 return (1);
 }
