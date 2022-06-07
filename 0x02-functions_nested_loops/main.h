0982bd2861b22a8e71a54e54ca4eb9a435a75156
Cabdalle <abnagsh@gmail.com>
2022-06-07 11:43:28 -0700

    Question 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,12 +8,12 @@ int _islower(int c);
 int _isalpa(int c);                                                                                                     
 int print_sign(in n);                                                                                                   
 int _abs(int);                                                                                                          
-int print_last_digit(int nld);
+int print_last_digit(int nld);                                                                                          
 void jack_bauer(void);                                                                                                  
 void times_table(void);                                                                                                 
 int add(int one, int two);                                                                                              
 void print_to_98(int n);                                                                                                
-void print_times_table(int n);                                                                                                            
-#endif                                                                                                                  
-
-/* main_h */
+void print_times_table(int n);                                                                                          
+                                                                                                                        
+#endif
+/* main_h */             
