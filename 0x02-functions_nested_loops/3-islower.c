bcf58d469fddb39a182d5b85f0ec25bac058c502
Cabdalle <abnagsh@gmail.com>
2022-06-07 15:44:25 -0700

    Question 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,6 +2,7 @@
 /**
  * _islower - tests where a character is a lower case.
  *Return: 1 if it is lower case letter.
+ * @c:character to test.
  * or 0 if it's not lower case
  */
 int _islower(int c)
