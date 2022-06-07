9fc6c89b65b798db9e5e238782298899dfaa580e
Cabdalle <abnagsh@gmail.com>
2022-06-07 16:07:15 -0700

    Question 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+ * _isalpha -tests if a character is from the english alphabet.
+ * @c:Character to be checked.
+ * Return:1 if th character is no and english character.
+ */
+int _isalpha(int c)
+
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'z'))
+	return (1);
+	else
+	return (0);
