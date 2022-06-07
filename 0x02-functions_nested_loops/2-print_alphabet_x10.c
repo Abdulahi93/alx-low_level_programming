0db0a1fe4ee2a441a75bd02120114c1e8b5f6c3a
Cabdalle <abnagsh@gmail.com>
2022-06-07 14:37:19 -0700

    Question 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ * print_alphabet_x10 - repeats the print alphabet 10 times.
+ * Return: null.
+*/
+void print_alpabet_x10(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x < 10; ++a)
+	{
+		for (y = 'a'; y <= 'z'; y++)
+		{
+			_putchar(y);
+		}
+		_putchar('\n')
+	}
+}
