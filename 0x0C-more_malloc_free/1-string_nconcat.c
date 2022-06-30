1c42ecf0c8baf759a7f639461a5fc422072e6f25
Cabdalle <abnagsh@gmail.com>
2022-06-30 09:21:23 -0700

    Question 0

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,85 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strlen_recursion - function that returns the length of a string
+ * @s: char pointer for string
+ * Return: int for length of string
+ */
+
+int _strlen_recursion(char *s)
+
+{
+	if (*s)
+		return (_strlen_recursion(s + 1) + 1);
+
+
+	return (0);
+}
+
+
+/*
+
+ * string_nconcat - function tat concatenates two strings with malloc'd memory
+
+ * @s1: Char pointer for string1
+
+ * @s2: Char pointer for string 
+
+ * @n: Unsigned int for memory size to be allocted
+
+ * Return: Char pointer of concatenated string pinted to a newly allocated
+
+ * space in memory, return NULL if function fails
+ */
+
+
+har *string_nconcat(char *s1, char *s2, unsigned int n)
+
+{
+	char *a;
+	unsigned int i, j, l1, l2, size;
+
+	if (s1 == NULL)
+		s1 = "";
+
+
+	if (s2 == NULL)
+		s2 = "";
+
+
+	l1 = _strlen_recursion(s1);
+	l2 = _strlen_recursion(s2);
+
+
+	if (n >= l2)
+		size = l1 + l2 + 1;
+
+
+	if (n < l2)
+		size = l1 + n + 1;
+
+
+	a = malloc(sizeof(char) * size);
+
+
+	if (a == NULL)
+		return (NULL);
+
+
+	for (i = 0; s1[i] != '\0'; i++)
+		a[i] = s1[i];
+
+
+	if (n >= l2)
+		for (j = 0; s2[j] != '\0'; j++)
+			a[i + j] = s2[j];
+
+
+	if (n < l2)
+		for (j = 0; j < n; j++)
+			a[i + j] = s2[j];
+
+	a[i + j] = '\0';
+	return (a);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,7 @@
+#ifndef _main_
+
+#define _main_
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+#endif
